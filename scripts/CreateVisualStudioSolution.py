9d378ffd4a54eb82e6936ce4216d8b5af3b12196
William Hjelm <william.hjelm@gmail.com>
2025-02-13 22:42:14 +0100

    Update gitignore and python script prints

--- scripts/CreateVisualStudioSolution.py
+++ scripts/CreateVisualStudioSolution.py
@@ -10,9 +10,3 @@ print("Dependencies satisfied!")
 print("Generating projects...")
 SetupProjects.RunPremake()
 PrintColor(Colors.OKGREEN, "Success! There should now be a .sln file in the root directory")
-
-if(not SetupDependencies.vulkanIsInstalled):
-    PrintColor(Colors.WARNING,
-               "However since Vulkan SDK was not found the code will likely not compile and run! " 
-               + "Please ensure you have installed it or alternatively ignore this if you believe this is incorrect "
-               + "I may have written bad vulkan check code as well")
